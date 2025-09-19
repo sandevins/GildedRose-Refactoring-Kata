fe87189650d5795784ced71500482f11e89058f3
sandevins <sdevicente@gradiant.org>
2025-09-19 13:09:50 +0200

    refactor: extract Sulfuras condition to top hierarchy

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -33,12 +33,13 @@ impl GildedRose {
 
     pub fn update_quality(&mut self) {
         for i in 0..self.items.len() {
+            if self.items[i].name == "Sulfuras, Hand of Ragnaros" {
+                continue;
+            }
             if self.items[i].name != "Aged Brie" && self.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
             {
                 if self.items[i].quality > 0 {
-                    if self.items[i].name != "Sulfuras, Hand of Ragnaros" {
-                        self.items[i].quality = self.items[i].quality - 1;
-                    }
+                    self.items[i].quality = self.items[i].quality - 1;
                 }
             } else {
                 if self.items[i].quality < 50 {
@@ -60,17 +61,14 @@ impl GildedRose {
                 }
             }
 
-            if self.items[i].name != "Sulfuras, Hand of Ragnaros" {
-                self.items[i].sell_in = self.items[i].sell_in - 1;
-            }
+            self.items[i].sell_in = self.items[i].sell_in - 1;
+            
 
             if self.items[i].sell_in < 0 {
                 if self.items[i].name != "Aged Brie" {
                     if self.items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
                         if self.items[i].quality > 0 {
-                            if self.items[i].name != "Sulfuras, Hand of Ragnaros" {
-                                self.items[i].quality = self.items[i].quality - 1;
-                            }
+                            self.items[i].quality = self.items[i].quality - 1;
                         }
                     } else {
                         self.items[i].quality = self.items[i].quality - self.items[i].quality;
