12f651758b660364d2e25e4dc0dc01308df03071
sandevins <sdevicente@gradiant.org>
2025-09-22 09:03:10 +0200

    chore: Remove useless test

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -78,16 +78,3 @@ impl GildedRose {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::{GildedRose, Item};
-
-    #[test]
-    pub fn foo() {
-        let items = vec![Item::new("foo", 0, 0)];
-        let mut rose = GildedRose::new(items);
-        rose.update_quality();
-
-        assert_eq!("foo", rose.items[0].name);
-    }
-}
